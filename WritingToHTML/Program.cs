dad52204c3f0ae403d2c4a2687e026c11d4b706d
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-29 23:25:21 +0100

    It works now!

--- WritingToHTML/Program.cs
+++ WritingToHTML/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace WritingToHTML
 {
@@ -10,6 +12,74 @@ namespace WritingToHTML
     {
         static void Main(string[] args)
         {
+            /* StringWriter sw = new StringWriter(@"C:\tmp\test\xml");
+            using (XmlTextWriter writer = new XmlTextWriter(sw))
+            {
+                sw.WriteLine("Test");
+            }*/
+
+            string sampleCreationString = @"
+        <html>
+        <head>
+        Head is here!
+        </head>
+
+        <body>
+        <h1>Sekcja 4</h1>
+        <Table>
+
+            <TR>
+
+                <TH> Broń </TH>
+
+                <TH> Moc<TH>
+                <TH> Element </TH>
+
+            </TR>
+
+            <TR>
+
+                <td> ApoCaster </td>
+
+                <td> 431 </ td>
+
+                <td> 0 </td>
+
+            </tr>
+
+            <tr>
+
+                <td> SpecterBuster </td>
+
+                <td> 560 </td>
+
+                <td> 150 </td>
+
+            </tr>
+
+            <tr>
+
+                <td> AmaranthineSlasher </td>
+
+                <td> 700 </td>
+
+                <td> 300 </td>
+
+            <tr>
+
+        </Table> 
+        </body>
+        </html>";
+
+
+
+            using (FileStream fs = new FileStream(@"C:\tmp\test.html", FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+                {
+                    sw.WriteLine(sampleCreationString);
+                }
+            }
         }
     }
 }
